71c0c5d80a774233561bbfab25547aec77487e15
유경아 <ukyunga0.1@gmail.com>
2026-07-03 17:57:00 +0900

    feat: 공통컴포넌트 프로세스 구현 완료 - 갯수별로 반응형 대응 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,45 @@
-// #PAGE: 병원소개 페이지
-export default function AboutPage() {
+import SubPageHero from '@/components/sections/SubPageHero';
+import ProcessSteps from '@/components/sections/ProcessSteps';
+
+export default function Home() {
     return (
         <main>
-            <h1>병원소개</h1>
+            <SubPageHero
+                title="NEW치과 소개"
+                desc={`정확한 진단과 정직한 진료로 \n오래도록 건강한 미소를 함께합니다.`}
+                bgImageName="bg_about"
+            />
+
+            <ProcessSteps
+                topSubtitle="Process"
+                topTitle="임플란트 과정"
+                items={[
+                    { id: '1', num: '01', text: '정밀 진단 및 \n상담 진행' },
+                    { id: '2', num: '02', text: '정확한 위치에 \n임플란트 식립' },
+                    { id: '3', num: '03', text: '안정적인 \n연결 시행' },
+                    { id: '4', num: '04', text: '치아기등과 심미성을 \n고려한 맞춤 보철' },
+                ]}
+            />
+            <ProcessSteps
+                topSubtitle="Process"
+                topTitle="임플란트 과정"
+                items={[
+                    { id: '1', num: '01', text: '정밀 진단 및 \n상담 진행' },
+                    { id: '2', num: '02', text: '정확한 위치에 \n임플란트 식립' },
+                    { id: '3', num: '03', text: '안정적인 \n연결 시행' },
+                ]}
+            />
+            <ProcessSteps
+                topSubtitle="Process"
+                topTitle="임플란트 과정"
+                items={[
+                    { id: '1', num: '01', text: '정밀 진단 및 \n상담 진행' },
+                    { id: '2', num: '02', text: '정확한 위치에 \n임플란트 식립' },
+                    { id: '3', num: '03', text: '안정적인 \n연결 시행' },
+                    { id: '4', num: '04', text: '치아기등과 심미성을 ' },
+                    { id: '5', num: '04', text: '치아기등과 심미성을 ' },
+                ]}
+            />
         </main>
     );
 }
