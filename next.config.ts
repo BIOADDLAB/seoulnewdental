2b69ea927f4bfbf35912902dbe8d7c99db231681
유경아 <ukyunga0.1@gmail.com>
2026-07-07 17:13:19 +0900

    fix: next.config에 unoptimized 옵션 추가하여 저화질 이슈 해결

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     images: {
-        unoptimized: true,
+        qualities: [75, 100],
     },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
